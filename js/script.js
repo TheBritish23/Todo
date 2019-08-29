9ec0a6d5922a4d0678e15103e9d189b916bdf977
Soro Cat <sorousha326@gmail.com>
2019-08-29 10:52:47 -0700

    add js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
 //business logic for List
+function List() {
+  this.what = []
+}
+
+List.prototype.listAll = function() {
+  $("#output").empty();
+  this.what.forEach(function(place){
+    $("#output").append(place.getTopics());
+  })
+}
+
 
 //user interface logic for Topics
 $(document).ready(function() {
